0cb7f9606cc3a8abd8e8d1460f4fa68e2f0a60d4
sid4py <sid.plasmabhai@gmail.com>
2025-10-24 13:30:37 -0400

    changes cred reader for config reorganization

--- neurobooth_explorer/credential_reader.py
+++ neurobooth_explorer/credential_reader.py
@@ -1,4 +1,4 @@
-from typing import Optional, List, Union, Literal
+from typing import Optional, List, Union, Tuple
 from enum import Enum
 import os
 import yaml
@@ -27,6 +27,10 @@ class databaseArgs(BaseModel):
     host: Union[str, IPvAnyAddress, AnyUrl]
     # this allows for values such as localhost or 127.0.0.1
     # or 192.168.xxx.xxx or <server_name>.nmr.mgh.harvard.edu
+    ssh_address_or_host: str # Eg. neurodoor.nmr.mgh.harvard.edu
+    ssh_pkey: str # should be path-to-file
+    remote_bind_address: Tuple[str, int] # Eg. ('192.168.100.1', 5432),
+    local_bind_address: Tuple[str, int] # Eg. ('localhost', 6543)
 
 
 class dataflowArgs(BaseModel):
@@ -34,6 +38,15 @@ class dataflowArgs(BaseModel):
     suitable_volumes: List[DirectoryPath]
     delete_threshold: float = Field(ge=0, le=1)
 # ----------------------------------------- #
+
+
+def get_server_hostname() -> str:
+    '''gets the name of the server on which the script is being run'''
+    try:
+        hostname = socket.gethostname().split('.')[0]
+        return hostname
+    except Exception as e:
+        raise Exception('Something went wrong in trying to get hostname')
 
 
 def read_environment_variable(environment_variable_name: str) -> os.PathLike:
@@ -62,7 +75,9 @@ def get_config_file_path(config_file_name: str,
         config_file_location = read_environment_variable(env_var_name)
     except Exception as e:
         raise Exception(f'Could not get config file location from environment variable {env_var_name}')
-
+    
+    config_environment = os.path.join('environments', get_server_hostname())
+    config_file_location = os.path.join(config_file_location, config_environment)
     config_fpath = os.path.join(config_file_location, config_file_name)
     validate_config_fpath(config_fpath)
     return config_fpath
@@ -81,6 +96,12 @@ def read_db_secrets(config_fpath: Optional[str] = None):
         'password' for the pg user password
         'host' for database host
 
+        Returns a second dictionary of SSH arguments with keys:
+        'ssh_address_or_host'
+        'ssh_pkey'
+        'remote_bind_address'
+        'local_bind_address'
+
         The credential file is assumed to be at a location which
         is defined in the EXPLORER_CONFIG_LOC environment variable
     '''
@@ -92,27 +113,20 @@ def read_db_secrets(config_fpath: Optional[str] = None):
     # Then load the config file
     db_config_dict = load_yaml_file_into_dict(config_fpath)
 
-    # Next get the name of server on which the script is being run
-    try:
-        hostname = socket.gethostname().split('.')[0]
-    except Exception as e:
-        raise Exception('Something went wrong in trying to get hostname')
-
-    if hostname not in db_config_dict.keys():
-        text = f'Hostname {hostname} does not match any key in config file {config_fpath}'
-        raise RuntimeError (text)
-    
-    # Now extract db args specific to host
-    host_specific_db_args = db_config_dict[hostname]
-
     # Then apply pydantic validation to db args values
-    db_args = databaseArgs(**host_specific_db_args)
+    db_args = databaseArgs(**db_config_dict)
 
-    credentials = {'database': db_args.db_name,
+    db_args_dict = {'database': db_args.db_name,
                    'user': db_args.db_user,
                    'password': db_args.password,
                    'host': db_args.host}
-    return credentials
+    
+    ssh_args_dict= {'ssh_address_or_host': db_args.ssh_address_or_host,
+                    'ssh_pkey': db_args.ssh_pkey,
+                    'remote_bind_address': db_args.remote_bind_address,
+                    'local_bind_address': db_args.local_bind_address}
+
+    return db_args_dict, ssh_args_dict
 
 
 def read_dataflow_configs(config_fpath: Optional[str] = None):
@@ -142,26 +156,16 @@ def read_dataflow_configs(config_fpath: Optional[str] = None):
     return dataflow_configs
 
 
-def get_ssh_args():
-    ''' SSH arguments don't change, hence are hardcoded here
-        and returned as is
-    '''
-    ssh_args = dict(
-            ssh_address_or_host='neurodoor.nmr.mgh.harvard.edu',
-            ssh_pkey='/space/neurobooth/1/applications/config/id_rsa', # this is user sp1022's id_rsa
-            remote_bind_address=('192.168.100.1', 5432),
-            local_bind_address=('localhost', 6543)
-    )
-    return ssh_args
-
-
 def get_user_pass_pairs():
-    '''Read username-password pairs from yml file''' 
-    config_fpath = get_config_file_path(user_pass_fname, env_var.USERPASS.value)
-    user_pass_dict = load_yaml_file_into_dict(config_fpath)
-
-    return user_pass_dict
-
+    '''Read username-password pairs from yml file'''
+    try:
+        config_file_location = read_environment_variable(env_var.USERPASS.value)
+        config_fpath = os.path.join(config_file_location, user_pass_fname)
+        validate_config_fpath(config_fpath)
+        user_pass_dict = load_yaml_file_into_dict(config_fpath)
+        return user_pass_dict
+    except:
+        raise Exception(f'Could not read user-password file!')
 
 
 if __name__ == '__main__':
@@ -171,13 +175,13 @@ if __name__ == '__main__':
     import sys
     
     if len(sys.argv)==3:
-        db_args = read_db_secrets(sys.argv[1])
+        db_args, ssh_args = read_db_secrets(sys.argv[1])
         dataflow_args = read_dataflow_configs(sys.argv[2])
     else:
         db_args = read_db_secrets()
         dataflow_args = read_dataflow_configs()
 
-    ssh_args = get_ssh_args()
+    # ssh_args = get_ssh_args()
     user_pass_pairs = get_user_pass_pairs()
 
     print()
